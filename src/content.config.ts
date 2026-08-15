4e9616960aa2c35578d1ebf007c058fdce29ab13
invenco-solder <leathersenseuk@gmail.com>
2026-08-15 14:56:36 +0100

    Update content.config.ts

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,17 +3,44 @@ import { defineCollection } from "astro:content";
 import { z } from "astro/zod";
 
 const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
 	loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
-	// Type-check frontmatter using a schema
 	schema: z.object({
 		title: z.string(),
 		description: z.string(),
-		// Transform string to Date object
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
 		heroImage: z.string().optional(),
 	}),
 });
 
-export const collections = { blog };
+const products = defineCollection({
+	loader: glob({
+		base: "./src/data/products",
+		pattern: "**/*.md",
+	}),
+	schema: z.object({
+		name: z.string(),
+		slug: z.string(),
+		description: z.string(),
+		price: z.number(),
+		image: z.string(),
+		images: z.array(z.string()).optional(),
+		category: z.string(),
+
+		specs: z.record(z.string()).optional(),
+
+		lengths: z
+			.array(
+				z.object({
+					name: z.string(),
+					price: z.number(),
+				})
+			)
+			.optional(),
+	}),
+});
+
+export const collections = {
+	blog,
+	products,
+};
